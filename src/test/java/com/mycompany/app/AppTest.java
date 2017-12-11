75c4974f0a53c9933bd3748d2065e90c17b98ed7
Emmanuel <emmanuelgeorge93@gmail.com>
2017-12-11 16:54:36 -0500

    second commit

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -3,35 +3,42 @@ package com.mycompany.app;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+    @Test
+    public void testCTORwithInt(){
+        LocalAccount tester = new LocalAccount(100.00);
+        assertEquals(100, tester.getBalance(), "account should have 100.00 in it");
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testCTOR(){
+        LocalAccount tester = new LocalAccount();
+        System.out.println("testAccount.txt is the test account");
+
+        tester.getAccount();
+        assertEquals(100, tester.getBalance(), "account should have 100.00 in it");
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testWithdraw(){
+        LocalAccount tester = new LocalAccount(100.00);
+        System.out.println("type 50 to test withdraw");
+        tester.withdraw();
+        assertEquals(50, tester.getBalance(), "account should have 50.00 in it");
+    }
+
+    @Test
+    public void testDeposit(){
+        LocalAccount tester = new LocalAccount(100.00);
+        System.out.println("type 50 to test deposit");
+        tester.deposit();
+        assertEquals(150, tester.getBalance(), "account should have 150.00 in it");
     }
 }
